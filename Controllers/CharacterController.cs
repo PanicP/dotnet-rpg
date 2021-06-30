65fff4a18c09c8d6df693d0d1e8fcf3c701d129b
PanicP <filmous2136@gmail.com>
2021-06-30 10:57:56 +0700

    WIP: Add Service to help Controller

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,18 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
 using dotnet_rpg.Models;
+using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class CharacterController : ControllerBase
-    {
-        private static Character knight = new Character();
+  [ApiController]
+  [Route("[controller]")]
+  public class CharacterController : ControllerBase
+  {
+    private readonly ICharacterService _characterService;
 
-        [HttpGet]
-        public ActionResult<Character> Get()
-        {
-            return Ok(knight);
-        }
+    public CharacterController(ICharacterService characterService)
+    {
+      _characterService = characterService;
+
     }
+
+    [HttpGet("GetAll")]
+    public ActionResult<List<Character>> Get()
+    {
+      return Ok(_characterService.GetAllCharacter());
+    }
+
+    [HttpGet("{id}")]
+
+    public ActionResult<Character> GetSingle(int id)
+    {
+      return Ok(_characterService.GetCharacterById(id));
+    }
+
+    [HttpPost]
+    public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+    {
+      return Ok(_characterService.AddCharacter(newCharacter));
+    }
+  }
 }
